c113771c7dd9f51306982b011488c272abe9a8f1
luis <nie.luyuan@gmail.com>
2017-05-17 17:15:23 -0700

    sfd

--- api.php
+++ api.php
@@ -1,12 +1,7 @@
 <?php
     require "vendor/autoload.php";
 
-    $app = new \Slim\Slim();
-    $app->get('/hello',function (){
-        echo "hello";
-    });
-
-    $app->post('/upload',function (){
+    function upload($name){
         $chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
         $length = 5;
         $filename = '';
@@ -14,15 +9,15 @@
         {
             $filename.=$chars[mt_rand(0, 36)];
         }
-        $allowedExts = array("mp3", "mp4", "wma");
+        $allowedExts = array("mp3", "mp4", "wma","jpg","png","jpeg");
 
         $target_dir = "uploads/";
-        $target_file = $target_dir .$filename.basename($_FILES["fileToUpload"]["name"]);
+        $target_file = $target_dir .$filename.basename($_FILES[$name]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            $check = getimagesize($_FILES[$name]["tmp_name"]);
             if($check !== false) {
                 echo "File is an image - " . $check["mime"] . ".";
                 $uploadOk = 1;
@@ -37,12 +32,12 @@
             $uploadOk = 0;
         }
 // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 5000000) {
+        if ($_FILES[$name]["size"] > 5000000) {
             echo "Sorry, your file is too large.";
             $uploadOk = 0;
         }
 // Allow certain file formats
-        if(!!!in_array($imageFileType,$allowedExts)){
+        if(!!!in_array(strtolower($imageFileType),$allowedExts)){
             echo "Sorry, only video files are allowed.";
             $uploadOk = 0;
         }
@@ -51,17 +46,24 @@
             echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
         } else {
-            /*$video = $ffmpeg->open($_FILES["fileToUpload"]["tmp_name"]);
-            $video
-                ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(10))
-                ->save('frame.jpg');*/
-            var_dump( $_FILES["fileToUpload"]["name"]);
-            /*if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            if (move_uploaded_file($_FILES[$name]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES[$name]["name"]). " has been uploaded.";
             } else {
                 echo "Sorry, there was an error uploading your file.";
-            }*/
+            }
         }
+    }
+    $app = new \Slim\Slim();
+    $app->get('/hello',function (){
+        echo "hello";
+    });
+
+    $app->post('/upload',function (){
+        if(file_exists($_FILES("fileToUpload"))){
+            upload("fileToUpload");
+        }
+
+        upload("imageToUpload");
     });
 
     $app->run();
